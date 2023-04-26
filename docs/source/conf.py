676c9fec074d5f2d99d41340c3c12e0e197de4d7
= <benjamin.loire@ensta-bretagne.org>
2023-04-26 15:45:57 +0200

    new theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,10 +23,10 @@ extensions = ["myst_parser",
 templates_path = ['_templates']
 exclude_patterns = []
 autoapi_dirs = ["../../src"]  
-
+autoapi_options = [ 'members', 'undoc-members', 'show-inheritance', 'show-module-summary', 'imported-members', ]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'cloud'
 html_static_path = ['_static']
